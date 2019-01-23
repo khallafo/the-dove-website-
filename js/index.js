4bd2df746a135b79cccd3d2e2d1e0b7f674958f9
khallafo <mahmoudhallaf288@gmail.com>
2019-01-23 23:37:16 +0200

    hosting company

---
+++ js/index.js
@@ -0,0 +1,18 @@
+//start our js
+$(document).ready(function () {
+    
+    
+    "use strict";
+    
+    
+    $("input").on("click",function(){
+        
+        $(this).css(
+            "border","2px solid #69C399",
+            
+            
+        );
+    });
+       
+  
+});
